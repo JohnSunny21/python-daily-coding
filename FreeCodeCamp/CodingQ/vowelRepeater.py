a81b1efef326de69833ef2f35581037fa352b2a5
JohnSunny <sunnyjohnballa@gmail.com>
2025-08-25 10:15:00 +0530

    feat(day 25): add vowel repeater with incremental duplication rules

---
+++ FreeCodeCamp/CodingQ/vowelRepeater.py
@@ -0,0 +1,37 @@
+"""
+Vowel Repeater
+Given a string, return a new version of the string where each vowel is duplicated one more time than the previous vowel you encountered. For instance, the first vowel in the sentence should remain unchanged. The second vowel should appear twice in a row. The third vowel should appear three times in a row, and so on.
+
+The letters a, e, i, o, and u, in either uppercase or lowercase, are considered vowels.
+The original vowel should keeps its case.
+Repeated vowels should be lowercase.
+All non-vowel characters should keep their original case.
+
+O/P ==>
+
+1. repeat_vowels("hello world") should return "helloo wooorld".
+2. repeat_vowels("freeCodeCamp") should return "freeeCooodeeeeCaaaaamp".
+3. repeat_vowels("AEIOU") should return "AEeIiiOoooUuuuu".
+4. repeat_vowels("I like eating ice cream in Iceland") should return "I liikeee eeeeaaaaatiiiiiing iiiiiiiceeeeeeee creeeeeeeeeaaaaaaaaaam iiiiiiiiiiin Iiiiiiiiiiiiceeeeeeeeeeeeelaaaaaaaaaaaaaand".
+"""
+
+def repeat_vowels(s):
+    vowels = "aeiou"
+
+    res = ""
+    count = 0
+    for char in s:
+        if char.lower() in vowels:
+            count += 1
+            res += char + (char.lower() * (count-1)) # The original character stays in its case without altering.
+        else:
+            res += char
+    
+    return res
+
+
+if __name__ == "__main__":
+    print(repeat_vowels("hello world"))
+    print(repeat_vowels("freeCodeCamp"))
+
+    
---
+++ FreeCodeCamp/CodingQ/vowelRepeaterTest.py
@@ -0,0 +1,22 @@
+import unittest
+from vowelRepeater import repeat_vowels
+
+
+class vowelRepeaterTest(unittest.TestCase):
+
+
+    def test1(self):
+        self.assertEqual(repeat_vowels("hello world"),"helloo wooorld")
+
+    def test2(self):
+        self.assertEqual(repeat_vowels("freeCodeCamp"),"freeeCooodeeeeCaaaaamp")
+
+    def test3(self):
+        self.assertEqual(repeat_vowels("AEIOU"),"AEeIiiOoooUuuuu")
+
+    def test4(self):
+        self.assertEqual(repeat_vowels("I like eating ice cream in Iceland"),"I liikeee eeeeaaaaatiiiiiing iiiiiiiceeeeeeee creeeeeeeeeaaaaaaaaaam iiiiiiiiiiin Iiiiiiiiiiiiceeeeeeeeeeeeelaaaaaaaaaaaaaand")
+
+
+if __name__ == "__main__":
+    unittest.main()
