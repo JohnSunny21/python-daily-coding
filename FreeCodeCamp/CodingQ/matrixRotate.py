ff25f7964719e272fe8ebc6d4b027b59da468d45
JohnSunny <sunnyjohnballa@gmail.com>
2025-08-27 10:25:00 +0530

    feat(day 27): add 90-degree matrix rotation via transpose and manual method

---
+++ FreeCodeCamp/CodingQ/matrixRotate.py
@@ -0,0 +1,43 @@
+"""
+Matrix Rotate
+Given a matrix (an array of arrays), rotate the matrix 90 degrees clockwise and return it. For instance, given [[1, 2], [3, 4]], which looks like this:
+
+1	2
+3	4
+You should return [[3, 1], [4, 2]], which looks like this:
+
+3	1
+4	2
+
+"""
+
+def rotate_builtin(matrix):
+
+    transposed = list(zip(*matrix))
+
+    rotated = [list(row[::-1]) for row in transposed]
+
+    return rotated
+
+def rotate(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    rotated = [[0]*rows for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+
+            rotated[j][rows-1-i] = matrix[i][j]
+
+    return rotated
+
+
+if __name__ == "__main__":
+    print(rotate([[1,2],[3,4]]))
+
+    print(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+    print(rotate_builtin([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    
+
---
+++ FreeCodeCamp/CodingQ/matrixRotateTest.py
@@ -0,0 +1,20 @@
+import unittest
+from matrixRotate import rotate
+
+class matrixRotateTest(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(rotate([[1]]),[[1]])
+
+    def test2(self):
+        self.assertEqual(rotate([[1, 2], [3, 4]]),[[3, 1], [4, 2]])
+
+    def test3(self):
+        self.assertEqual(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),[[7, 4, 1], [8, 5, 2], [9, 6, 3]])
+
+    def test4(self):
+        self.assertEqual(rotate([[0, 1, 0], [1, 0, 1], [0, 0, 0]]),[[0, 1, 0], [0, 0, 1], [0, 1, 0]])
+
+
+if __name__ == "__main__":
+    unittest.main()
